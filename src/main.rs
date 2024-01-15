b5c64402a50104d5013565dafe8d072a82d55b87
Ryan Brewer <ryanbrew317@gmail.com>
2024-01-15 15:58:30 -0800

    clean up

--- src/main.rs
+++ src/main.rs
@@ -10,79 +10,79 @@ use crate::header::pretty_op2;
 
 fn main() {
     let code: Vec<u8> = vec![
+        0x00,   // _op_        _run-time_stack_    _compile-time_stack_
         0x00, 
         0x00, 
         0x00, 
-        0x00, 
-        0x01,   // region               r
-        0x12,   // ct_get 0             r,r
+        0x01,   // region                           r
+        0x12,   // ct_get 0                         r,r
         0x00,
-        0x06,   // read                 {+r},r
-        0x04,   // cap_le               {c≤+r},r
-        0x12,   // ct_get 0             {c≤+r},{c≤+r},r
+        0x06,   // read                             {+r},r
+        0x04,   // cap_le                           {c≤+r},r
+        0x12,   // ct_get 0                         {c≤+r},{c≤+r},r
         0x00,
-        0x00,   // req                  {c≤+r},r
-        0x0E,   // all                  a,{c≤+r},r
-        0x12,   // ct_get 0             a,a,{c≤+r},r
+        0x00,   // req                              {c≤+r},r
+        0x0E,   // all                              a,{c≤+r},r
+        0x12,   // ct_get 0                         a,a,{c≤+r},r
         0x00,
-        0x00,   // req          a       a,{c≤+r},r
-        0x12,   // ct_get 2     a       r,a,{c≤+r},r
+        0x00,   // req          a                   a,{c≤+r},r
+        0x12,   // ct_get 2     a                   r,a,{c≤+r},r
         0x02,
-        0x0F,   // some         a       b,r,a,{c≤+r},r
-        0x12,   // ct_get 3     a       {c≤+r},b,r,a,{c≤+r},r
+        0x0F,   // some         a                   b,r,a,{c≤+r},r
+        0x12,   // ct_get 3     a                   {c≤+r},b,r,a,{c≤+r},r
         0x03,
-        0x12,   // ct_get 5     a       r,{c≤+r},b,r,a,{c≤+r},r
+        0x12,   // ct_get 5     a                   r,{c≤+r},b,r,a,{c≤+r},r
         0x05,
-        0x12,   // ct_get 4     a       a,r,{c≤+r},b,r,a,{c≤+r},r
+        0x12,   // ct_get 4     a                   a,r,{c≤+r},b,r,a,{c≤+r},r
         0x04,
-        0x12,   // ct_get 0     a       a,a,r,{c≤+r},b,r,a,{c≤+r},r
+        0x12,   // ct_get 0     a                   a,a,r,{c≤+r},b,r,a,{c≤+r},r
         0x00,
-        0x0C,   // tuple 2      a       a*a@r,{c≤+r},b,r,a,{c≤+r},r
+        0x0C,   // tuple 2      a                   a*a@r,{c≤+r},b,r,a,{c≤+r},r
         0x02,
-        0x12,   // ct_get 2     a       b,a*a@r,{c≤+r},b,r,a,{c≤+r},r
+        0x12,   // ct_get 2     a                   b,a*a@r,{c≤+r},b,r,a,{c≤+r},r
         0x02,
-        0x11,   // func 2       a       [{c≤+r}](b,a*a@r),b,r,a,{c≤+r},r
+        0x11,   // func 2       a                   [{c≤+r}](b,a*a@r),b,r,a,{c≤+r},r
         0x02,
-        0x0C,   // tuple 2      a       [{c≤+r}](b,a*a@r)*b@r,a,{c≤+r},r
+        0x0C,   // tuple 2      a                   [{c≤+r}](b,a*a@r)*b@r,a,{c≤+r},r
         0x02,
-        0x10,   // emos         a       some b.[{c≤+r}](b,a*a@r)*b@r,a,{c≤+r},r
-        0x00,   // req          a,k     a,{c≤+r},r
-        0x12,   // ct_get 2     a,k     r,a,{c≤+r},r
+        0x10,   // emos         a                   some b.[{c≤+r}](b,a*a@r)*b@r,a,{c≤+r},r
+        0x00,   // req          a,k                 a,{c≤+r},r
+        0x12,   // ct_get 2     a,k                 r,a,{c≤+r},r
         0x02,
-        0x08,   // handle       a,k     handle(r),a,{c≤+r},r
-        0x00,   // req          a,k,r   a,{c≤+r},r
-        0x12,   // ct_get 2     a,k,r   r,a,{c≤+r},r
+        0x08,   // handle       a,k                 handle(r),a,{c≤+r},r
+        0x00,   // req          a,k,r               a,{c≤+r},r
+        0x12,   // ct_get 2     a,k,r               r,a,{c≤+r},r
         0x02,
-        0x12,   // ct_get 1     a,k,r   a,r,a,{c≤+r},r
+        0x12,   // ct_get 1     a,k,r               a,r,a,{c≤+r},r
         0x01,
-        0x12,   // ct_get 0     a,k,r   a,a,r,a,{c≤+r},r
+        0x12,   // ct_get 0     a,k,r               a,a,r,a,{c≤+r},r
         0x00,
-        0x0C,   // tuple 2      a,k,r   a*a@r,a,{c≤+r},r
+        0x0C,   // tuple 2      a,k,r               a*a@r,a,{c≤+r},r
         0x02,
-        0x15,   // get 2        r,a,k,r a*a@r,a,{c≤+r},r
+        0x15,   // get 2        r,a,k,r             a*a@r,a,{c≤+r},r
         0x02,
-        0x17,   // malloc
-        0x15,   // get 1
+        0x17,   // malloc       (_,_),a,k,r         a,{c≤+r},r
+        0x15,   // get 1        a,(_,_),a,k,r       a,{c≤+r},r
         0x01,
-        0x16,   // init 0
+        0x16,   // init 0       (a,_),a,k,r         a,{c≤+r},r
         0x01,
-        0x15,   // get 1
+        0x15,   // get 1        a,(a,_),a,k,r       a,{c≤+r},r
         0x01,
-        0x16,   // init 1
+        0x16,   // init 1       (a,a),a,k,r         a,{c≤+r},r
         0x01,
-        0x15,   // get 2
+        0x15,   // get 2        k,(a,a),a,k,r       a,{c≤+r},r
         0x02,
-        0x14,   // unpack
-        0x18,   // proj 1
+        0x14,   // unpack       (f,c),(a,a),a,k,r   a,{c≤+r},r
+        0x18,   // proj 1       c,(a,a),a,k,r       a,{c≤+r},r
         0x01,
-        0x15,   // get 3
+        0x15,   // get 3        k,c,(a,a),a,k,r     a,{c≤+r},r
         0x03,
-        0x14,   // unpack
-        0x18,   // proj 0
+        0x14,   // unpack       (f,c),c,(a,a),a,k,r a,{c≤+r},r
+        0x18,   // proj 0       f,c,(a,a),a,k,r     a,{c≤+r},r
         0x00,
-        0x19,   // clean 3
+        0x19,   // clean 3      f,c,(a,a)           a,{c≤+r},r
         0x03,
-        0x1A    // call
+        0x1A    // call         --                  --
     ];
     let prog = parse::go(&code);
     match prog {
@@ -110,46 +110,3 @@ fn main() {
         }
     }
 }
-
-                // vec![
-                //     Op1Region(),
-                //     Op1Cap(),
-                //     Op1CTGet(0),
-                //     Op1Req(),
-                //     Op1All(),
-                //     Op1CTGet(0),
-                //     Op1Req(),
-                //     Op1CTGet(2),
-                //     Op1Some(),
-                //     Op1CTGet(3),
-                //     Op1CTGet(5),
-                //     Op1CTGet(4),
-                //     Op1CTGet(0),
-                //     Op1Tuple(2),
-                //     Op1CTGet(2),
-                //     Op1Func(2),
-                //     Op1Tuple(2),
-                //     Op1Emos(),
-                //     Op1Req(),
-                //     Op1CTGet(2),
-                //     Op1Handle(),
-                //     Op1Req(),
-                //     Op1CTGet(2),
-                //     Op1CTGet(1),
-                //     Op1CTGet(0),
-                //     Op1Tuple(2),
-                //     Op1Get(2),
-                //     Op1Malloc(),
-                //     Op1Get(1),
-                //     Op1Init(0),
-                //     Op1Get(1),
-                //     Op1Init(1),
-                //     Op1Get(2),
-                //     Op1Unpack(),
-                //     Op1Proj(1),
-                //     Op1Get(3),
-                //     Op1Unpack(),
-                //     Op1Proj(0),
-                //     Op1Clean(3),
-                //     Op1Call(),
-                // ],
